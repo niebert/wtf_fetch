8ae141f33bf76fcfb0340c51ed964bd7ea036984
Engelbert Niehaus <niehaus@uni-landau.de>
2018-07-30 11:06:06 +0200

    dependency to codacy removed until implemented correctly

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -1,5 +1,5 @@
 var exec = require('shelljs').exec;
-var nyc = './node_modules/nyc/bin/nyc.js';
+//var nyc = './node_modules/nyc/bin/nyc.js';
 var codacity = './node_modules/.bin/codacy-coverage';
 var tape = './node_modules/tape/bin/tape';
 var test = tape + ' "./tests/**/*.test.js" ';
@@ -11,10 +11,10 @@ obf.env['CODACY_PROJ' + mid + 'EN'] = '07903b1db5f440a2a3caf49fc40ba995'; //i do
 //run all the tests
 console.log('\n 🏃  running coverage tests..');
 // exec(nyc + ' --reporter=text-lcov ' + test + ' > coverage.lcov');
-exec(nyc + ' --reporter=html ' + test + ' | tap-min');
+//exec(nyc + ' --reporter=html ' + test + ' | tap-min');
 
 //publish results for codacity
 console.log('\n\n\nPublishing results to codacity...\n');
 var cmd = nyc + ' report --reporter=text-lcov ' + test + ' | ' + codacity;
-exec(cmd);
+// exec(cmd);
 console.log('\n 🏃 done!');
