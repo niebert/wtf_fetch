97635807623cd7231aa6fadc0889a336d37575f4
Engelbert Niehaus <niehaus@uni-landau.de>
2018-07-30 11:09:41 +0200

    package.json update coverage.js updated

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -5,16 +5,20 @@ var tape = './node_modules/tape/bin/tape';
 var test = tape + ' "./tests/**/*.test.js" ';
 
 //to upload to codacity, set the api key as $CODACY_PROJECT_TOKEN
+
+/*
 var obf = process;
 var mid = 'ECT_TOK';
 obf.env['CODACY_PROJ' + mid + 'EN'] = '07903b1db5f440a2a3caf49fc40ba995'; //i don't really care if you steal this.
 //run all the tests
 console.log('\n 🏃  running coverage tests..');
 // exec(nyc + ' --reporter=text-lcov ' + test + ' > coverage.lcov');
-//exec(nyc + ' --reporter=html ' + test + ' | tap-min');
+exec(nyc + ' --reporter=html ' + test + ' | tap-min');
 
 //publish results for codacity
 console.log('\n\n\nPublishing results to codacity...\n');
 var cmd = nyc + ' report --reporter=text-lcov ' + test + ' | ' + codacity;
-// exec(cmd);
+exec(cmd);
+*/
+
 console.log('\n 🏃 done!');
